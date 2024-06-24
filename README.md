5dfb6633346cd194451a1ca584326465b6708bb0
Jake Soenneker <jake@soenneker.com>
2024-06-23 20:04:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/soenneker.blazor.utils.resourceloader.svg?style=for-the-badge)](https://www.nuget.org/packages/soenneker.blazor.utils.resourceloader/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.blazor.utils.resourceloader/publish-package.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.blazor.utils.resourceloader/actions/workflows/publish-package.yml)
+[![](https://img.shields.io/nuget/dt/soenneker.blazor.utils.resourceloader.svg?style=for-the-badge)](https://www.nuget.org/packages/soenneker.blazor.utils.resourceloader/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Blazor.Utils.ResourceLoader
+### A Blazor JavaScript module for dynamically loading scripts and styles
+
+## Installation
+
+```
+dotnet add package Soenneker.Blazor.Utils.ResourceLoader
+```
---
+++ src/wwwroot/resourceloader.js
@@ -0,0 +1,64 @@
+﻿export class ResourceLoader {
+    constructor() {
+        this.loadedScripts = {};
+        this.loadedCss = {};
+    }
+
+    loadScript(url, integrity, crossorigin = 'anonymous') {
+        if (this.loadedScripts[url] === true)
+            return;
+
+        this.loadedScripts[url] = true;
+
+        return new Promise((resolve, reject) => {
+            const script = document.createElement('script');
+            script.src = url;
+
+            if (crossorigin)
+                script.crossOrigin = crossorigin;
+
+            if (integrity)
+                script.integrity = integrity;
+            
+            script.onload = () => resolve();
+            script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+            document.head.appendChild(script);
+        });
+    }
+
+    loadCss(url, integrity, crossorigin = 'anonymous') {
+        if (this.loadedCss[url] === true)
+            return;
+
+        this.loadedCss[url] = true;
+
+        return new Promise((resolve, reject) => {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = url;
+
+            if (crossorigin)
+                link.crossOrigin = crossorigin;
+
+            if (integrity)
+                link.integrity = integrity;
+            
+            link.onload = () => resolve();
+            link.onerror = () => reject(new Error(`Failed to load stylesheet: ${url}`));
+            document.head.appendChild(link);
+        });
+    }
+
+    waitForVariable(variableName, interval = 100) {
+        return new Promise((resolve) => {
+            const checkVariable = setInterval(() => {
+                if (typeof window[variableName] !== 'undefined' && window[variableName] !== null) {
+                    clearInterval(checkVariable);
+                    resolve(window[variableName]);
+                }
+            }, interval);
+        });
+    }
+}
+
+window.ResourceLoader = new ResourceLoader();
