780221da5b0c9dcb5e81069186abd2e30c78bfab
Jake Soenneker <jake@soenneker.com>
2024-06-30 06:50:29 -0500

    module loading added

--- src/Abstract/IResourceLoader.cs
+++ src/Abstract/IResourceLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using System.Threading;
+using Microsoft.JSInterop;
 
 namespace Soenneker.Blazor.Utils.ResourceLoader.Abstract;
 
@@ -16,7 +17,43 @@ public interface IResourceLoader
     /// <param name="integrity">The integrity hash of the script for Subresource Integrity (SRI) validation.</param>
     /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
     /// <returns>A task that represents the asynchronous operation. The task will complete when the script is loaded or if it has already been loaded.</returns>
-    ValueTask LoadScript(string uri, string integrity, CancellationToken cancellationToken = default);
+    ValueTask LoadScript(string uri, string? integrity = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Loads a script from the specified URI and waits until the specified JavaScript variable is available.
+    /// </summary>
+    /// <param name="uri">The URI of the script to load.</param>
+    /// <param name="variableName">The name of the JavaScript variable to wait for.</param>
+    /// <param name="integrity">The integrity hash of the script for Subresource Integrity (SRI) validation. This parameter is optional.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task completes when the script is loaded and the variable is available or the operation is cancelled.</returns>
+    ValueTask LoadScriptAndWaitForVariable(string uri, string variableName, string? integrity = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Imports a JavaScript module by its name.
+    /// </summary>
+    /// <param name="name">The name of the JavaScript module to import.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the imported JavaScript module reference.</returns>
+    ValueTask<IJSObjectReference> ImportModule(string name, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Waits until the specified module is loaded.
+    /// </summary>
+    /// <param name="name">The name of the JavaScript module.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    ValueTask ImportModuleAndWait(string name, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Waits until the specified JavaScript module is loaded and JS variable is available (not undefined).
+    /// </summary>
+    /// <param name="name">The name of the script or style to wait for.</param>
+    /// <param name="variableName">The name of the JavaScript variable to check for availability.</param>
+    /// <param name="delay">The delay in milliseconds between each check for the variable's availability. The default is 100 milliseconds.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task completes when the variable is available or the operation is cancelled.</returns>
+    ValueTask ImportModuleAndWaitUntilAvailable(string name, string variableName, int delay = 100, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Loads a style from the specified URI if it hasn't already been loaded  (through the ResourceLoader)
@@ -25,7 +62,7 @@ public interface IResourceLoader
     /// <param name="integrity">The integrity hash of the style for Subresource Integrity (SRI) validation.</param>
     /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
     /// <returns>A task that represents the asynchronous operation. The task will complete when the style is loaded or if it has already been loaded.</returns>
-    ValueTask LoadStyle(string uri, string integrity, CancellationToken cancellationToken = default);
+    ValueTask LoadStyle(string uri, string? integrity = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Asynchronously waits until a specified JavaScript variable is available in the global scope.
--- src/ResourceLoader.cs
+++ src/ResourceLoader.cs
@@ -15,6 +15,7 @@ namespace Soenneker.Blazor.Utils.ResourceLoader;
 public class ResourceLoader : IResourceLoader
 {
     private readonly IJSRuntime _jsRuntime;
+    private readonly IModuleImportUtil _moduleImportUtil;
     private readonly IJsVariableInterop _jsVariableInterop;
     private readonly ConcurrentDictionary<string, Task> _loadingScripts = new();
     private readonly ConcurrentDictionary<string, Task> _loadingStyles = new();
@@ -24,37 +25,59 @@ public class ResourceLoader : IResourceLoader
     public ResourceLoader(IJSRuntime jsRuntime, IModuleImportUtil moduleImportUtil, IJsVariableInterop jsVariableInterop)
     {
         _jsRuntime = jsRuntime;
+        _moduleImportUtil = moduleImportUtil;
         _jsVariableInterop = jsVariableInterop;
 
         _scriptInitializer = new AsyncSingleton<object>(async objects => {
 
             var cancellationToken = (CancellationToken)objects[0];
 
-            await moduleImportUtil.Import("Soenneker.Blazor.Utils.ResourceLoader/resourceloader.js", cancellationToken);
-            await moduleImportUtil.WaitUntilLoadedAndAvailable("Soenneker.Blazor.Utils.ResourceLoader/resourceloader.js", "ResourceLoader", 100, cancellationToken);
+            await _moduleImportUtil.Import("Soenneker.Blazor.Utils.ResourceLoader/resourceloader.js", cancellationToken);
+            await _moduleImportUtil.ImportAndWaitUntilAvailable("Soenneker.Blazor.Utils.ResourceLoader/resourceloader.js", "ResourceLoader", 100, cancellationToken);
 
             return new object();
         });
     }
 
-    public async ValueTask LoadScript(string uri, string integrity, CancellationToken cancellationToken = default)
+    public async ValueTask LoadScript(string uri, string? integrity = null, CancellationToken cancellationToken = default)
     {
         _ = await _scriptInitializer.Get(cancellationToken).NoSync();
         await _loadingScripts.GetOrAdd(uri, _ => LoadScriptInternal(uri, integrity, cancellationToken)).NoSync();
     }
 
-    public async ValueTask LoadStyle(string uri, string integrity, CancellationToken cancellationToken = default)
+    public async ValueTask LoadScriptAndWaitForVariable(string uri, string variableName, string? integrity = null, CancellationToken cancellationToken = default)
+    {
+        await LoadScript(uri, integrity, cancellationToken).NoSync();
+        await WaitForVariable(variableName, cancellationToken: cancellationToken).NoSync();
+    }
+
+    public async ValueTask LoadStyle(string uri, string? integrity, CancellationToken cancellationToken = default)
     {
         _ = await _scriptInitializer.Get(cancellationToken).NoSync();
         await _loadingStyles.GetOrAdd(uri, _ => LoadStyleInternal(uri, integrity, cancellationToken)).NoSync();
     }
 
-    private Task LoadScriptInternal(string uri, string integrity, CancellationToken cancellationToken)
+    public ValueTask<IJSObjectReference> ImportModule(string name, CancellationToken cancellationToken = default)
+    {
+        return _moduleImportUtil.Import(name, cancellationToken);
+    }
+
+    public ValueTask ImportModuleAndWait(string name, CancellationToken cancellationToken = default)
+    {
+        return _moduleImportUtil.ImportAndWait(name, cancellationToken);
+    }
+
+    public ValueTask ImportModuleAndWaitUntilAvailable(string name, string variableName, int delay = 100, CancellationToken cancellationToken = default)
+    {
+        return _moduleImportUtil.ImportAndWaitUntilAvailable(name, variableName, delay, cancellationToken);
+    }
+
+    private Task LoadScriptInternal(string uri, string? integrity, CancellationToken cancellationToken)
     {
         return _jsRuntime.InvokeVoidAsync("ResourceLoader.loadScript", cancellationToken, uri, integrity).AsTask();
     }
 
-    private Task LoadStyleInternal(string uri, string integrity, CancellationToken cancellationToken)
+    private Task LoadStyleInternal(string uri, string? integrity, CancellationToken cancellationToken)
     {
         return _jsRuntime.InvokeVoidAsync("ResourceLoader.loadStyle", cancellationToken, uri, integrity).AsTask();
     }
