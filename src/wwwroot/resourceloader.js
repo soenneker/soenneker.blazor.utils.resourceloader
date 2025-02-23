619e12665787bb393b89e610de80951c78941f4e
Jake Soenneker <jake@soenneker.com>
2025-02-22 19:53:58 -0600

    Improves performance for resource loading

--- src/wwwroot/resourceloader.js
+++ src/wwwroot/resourceloader.js
@@ -1,65 +1,60 @@
 ﻿export class ResourceLoader {
     constructor() {
-        this.loadedScripts = {};
-        this.loadedCss = {};
+        this.loadedScripts = new Map();
+        this.loadedCss = new Map();
     }
 
-    loadScript(url, integrity, crossorigin, loadInHead, async, defer) {
-        if (this.loadedScripts[url] === true)
-            return;
+    loadScript(url, integrity, crossorigin, loadInHead = false, async = false, defer = false) {
+        if (this.loadedScripts.has(url))
+            return this.loadedScripts.get(url);
 
-        this.loadedScripts[url] = true;
+        const script = document.createElement('script');
+        script.src = url;
 
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = url;
+        if (crossorigin)
+            script.crossOrigin = crossorigin;
 
-            if (crossorigin)
-                script.crossOrigin = crossorigin;
+        if (integrity)
+            script.integrity = integrity;
 
-            if (integrity)
-                script.integrity = integrity;
+        script.async = !!async;
+        script.defer = !!defer;
 
-            if (async)
-                script.async = true;
-
-            if (defer)
-                script.defer = true;
-
-            script.onload = () => resolve();
-            script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
-
-            if (loadInHead)
-                document.head.appendChild(script);
-            else
-                document.body.appendChild(script);
+        const promise = new Promise((resolve, reject) => {
+            script.onload = () => resolve(url);
+            script.onerror = (err) => reject(new Error(`Failed to load script: ${url} - ${err.message}`));
         });
+
+        this.loadedScripts.set(url, promise);
+        (loadInHead ? document.head : document.body).appendChild(script);
+        return promise;
     }
 
-    loadStyle(url, integrity, crossorigin, media, type) {
-        if (this.loadedCss[url] === true)
-            return;
+    loadStyle(url, integrity, crossorigin, media = "all", type = "text/css") {
+        if (this.loadedCss.has(url))
+            return this.loadedCss.get(url);
 
-        this.loadedCss[url] = true;
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = url;
 
-        return new Promise((resolve, reject) => {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = url;
+        if (crossorigin)
+            link.crossOrigin = crossorigin;
 
-            if (crossorigin)
-                link.crossOrigin = crossorigin;
+        if (integrity)
+            link.integrity = integrity;
 
-            if (integrity)
-                link.integrity = integrity;
+        link.media = media;
+        link.type = type;
 
-            link.media = media;
-            link.type = type;
-
-            link.onload = () => resolve();
-            link.onerror = () => reject(new Error(`Failed to load stylesheet: ${url}`));
-            document.head.appendChild(link);
+        const promise = new Promise((resolve, reject) => {
+            link.onload = () => resolve(url);
+            link.onerror = (err) => reject(new Error(`Failed to load stylesheet: ${url} - ${err.message}`));
         });
+
+        this.loadedCss.set(url, promise);
+        document.head.appendChild(link);
+        return promise;
     }
 }
 
